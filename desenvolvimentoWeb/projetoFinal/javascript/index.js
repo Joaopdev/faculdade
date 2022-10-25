ec9f14a52a8692555cedbfee40632342050feaa8
Joao Paulo <joaotst@hotmail.com>
2022-10-25 00:59:57 -0300

    add todos os tracks dos trabalhos ate esse momento e apos removerei o .git deste projeto para criar outro

---
+++ desenvolvimentoWeb/projetoFinal/javascript/index.js
@@ -0,0 +1,8 @@
+let formularioCategoria = document.getElementsByClassName("formularioCategoria");
+let formularioFornecedor = document.getElementsByClassName("formularioFornecedor");
+let formularioPecas = document.getElementsByClassName("formularioPecas");
+
+
+
+function controlaExibicaoFormularios() {}
+
